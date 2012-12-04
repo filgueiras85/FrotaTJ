257da5a807bca2569969d85388ae58b910f14bf0
Luiz Octavio B. Amand <amand6@hotmail.com>
2012-12-04 02:34:22 -0200

    Telefone

--- frotaTJ/src/mb/MBFornecedor.java
+++ frotaTJ/src/mb/MBFornecedor.java
@@ -7,6 +7,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import dao.Fornecedor;
 import dao.FornecedorDAO;
 
@@ -56,6 +58,35 @@ public class MBFornecedor {
 		}
 		return retorno;
 	}
+	
+	
+	public String cadastrarTelefone (String tel){
+		boolean validar = false;
+		if (validar == true){
+		return tel;
+		} else {
+			return null;
+		}
+	}
+
+	
+	public static  String validarTelefone(String tel) {  
+		  
+	      String formato = "\\([0-9]{2}?\\)[0-9]{4}?\\-[0-9]{4}";
+	      String formato2 = "\\([0-9]{2}?\\)[0-9]{5}?\\-[0-9]{4}";
+	      String formato3 = "\\([0-9]{2}?\\)[0-9]{4}?\\-[0-9]{5}";
+	      String validar = "ok";  
+	      if(tel == null || tel.matches(formato3) || tel.matches(formato2) || tel.matches(formato)){  
+	    	  return validar;  
+	      } else {  
+	    	  JOptionPane.showMessageDialog(null,"Cadastro de número telefônico incorreto! /n O formato do número deve ser (DDD) 1111-1111.");
+		    
+		 
+	      }
+		return null;
+	   }  
+	
+	  
 	
 	public Fornecedor retornarFornecedor(int id) {
 		FornecedorDAO daoFornecedor = FornecedorDAO.getInstance();
