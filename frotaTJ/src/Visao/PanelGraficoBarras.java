93180b62a922d75ecd3d0ac17511acfdb78ee200
André Manoel Albani Cezar <manoelalbani@gmail.com>
2013-04-09 20:08:31 -0300

    Grafico Barra OK, Falta colocar na tela principal
    qnd chegar em casa eu coloco e faço salvar

--- frotaTJ/src/Visao/PanelGraficoBarras.java
+++ frotaTJ/src/Visao/PanelGraficoBarras.java
@@ -1,6 +1,12 @@
 package Visao;
 import java.awt.Dimension;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.swing.JFrame;
+
+import mb.MBTipoServiçoModelo;
+import mb.MBVeiculo;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -9,10 +15,16 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 
+import dao.TipoServicoModelo;
+import dao.Veiculo;
+
 
 public class PanelGraficoBarras extends JFrame{
 
-public PanelGraficoBarras(String title) {
+	final static MBVeiculo mbVeiculo = MBVeiculo.getInstance();
+	final static MBTipoServiçoModelo tipoServicoModeloMB = MBTipoServiçoModelo.getInstance();
+
+public PanelGraficoBarras(String title) throws ClassNotFoundException, SQLException {
 super(title);
 CategoryDataset dataset = PanelGraficoBarras.createDataset();
 JFreeChart chart = PanelGraficoBarras.createBarChart(dataset);
@@ -21,17 +33,30 @@ panel.setPreferredSize(new Dimension(400, 300));
 setContentPane(panel);
 }
 
-private static CategoryDataset createDataset() {
-DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-dataset.addValue(6, "Preto", "Corsa");
-dataset.addValue(4, "Preto", "Fiesta");
-dataset.addValue(3, "Preto", "Gol");
-dataset.addValue(5, "Vermelho", "Corsa");
-dataset.addValue(2, "Vermelho", "Fiesta");
-dataset.addValue(3, "Vermelho", "Gol");
-dataset.addValue(2, "Azul", "Corsa");
-dataset.addValue(8, "Azul", "Fiesta");
-dataset.addValue(1, "Azul", "Gol");
+private static CategoryDataset createDataset() throws ClassNotFoundException, SQLException {
+	List<Veiculo> veiculos = (List<Veiculo>) mbVeiculo.listarVeiculos();
+	DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+	List<TipoServicoModelo> tipoServicos = (List<TipoServicoModelo>) tipoServicoModeloMB.listarTipoServicosModelos();
+	for(int j = 0; j<tipoServicos.size();j++){
+		dataset.addValue(0, "verde", tipoServicos.get(j).getTipoServico().getNome());
+		dataset.addValue(0, "amarelo",tipoServicos.get(j).getTipoServico().getNome());
+		dataset.addValue(0, "vermelho", tipoServicos.get(j).getTipoServico().getNome());
+
+
+	}
+
+	for(int ie=0;ie<veiculos.size();ie++){
+	 List<TipoServicoModelo> tiposServicosModeloVeiculo = (List<TipoServicoModelo>) tipoServicoModeloMB.findTipoServicoByModelo(veiculos.get(ie));
+		for (int i=0;i<tiposServicosModeloVeiculo.size();i++){
+									
+				
+			 dataset.incrementValue(1, tiposServicosModeloVeiculo.get(i).getSituacao(),tiposServicosModeloVeiculo.get(i).getTipoServico().getNome()) ; // servico a fazer
+						
+										
+			}	
+	}
+
+
 return dataset;
 }
 
@@ -46,7 +71,7 @@ true, false, false); // exibir: legendas, tooltips, url
 return chart;
 }
 
-public static void main( String[] args ) {
+public static void main( String[] args ) throws ClassNotFoundException, SQLException {
 	PanelGraficoBarras chart = new PanelGraficoBarras("Teste Bar Chart");
 chart.pack();
 chart.setVisible(true);
