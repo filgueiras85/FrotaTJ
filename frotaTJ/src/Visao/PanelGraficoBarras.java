2facea3e9331922dacd1ebc1202da51bfd5f5cad
André Manoel Albani Cezar <manoelalbani@gmail.com>
2013-04-10 17:38:25 -0300

    Grafico Barra

--- frotaTJ/src/Visao/PanelGraficoBarras.java
+++ frotaTJ/src/Visao/PanelGraficoBarras.java
@@ -1,6 +1,12 @@
 package Visao;
 import java.awt.Dimension;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -10,6 +16,7 @@ import mb.MBVeiculo;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.CategoryDataset;
@@ -24,10 +31,37 @@ public class PanelGraficoBarras extends JFrame{
 	final static MBVeiculo mbVeiculo = MBVeiculo.getInstance();
 	final static MBTipoServiçoModelo tipoServicoModeloMB = MBTipoServiçoModelo.getInstance();
 
-public PanelGraficoBarras(String title) throws ClassNotFoundException, SQLException {
+public PanelGraficoBarras(String title) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
 super(title);
 CategoryDataset dataset = PanelGraficoBarras.createDataset();
 JFreeChart chart = PanelGraficoBarras.createBarChart(dataset);
+StringBuffer strNome = new StringBuffer();
+strNome.append("imagens\\"); 
+
+
+DateFormat formatter = new SimpleDateFormat("ddMMyyyyhhmmss");
+
+String Nome = null;
+
+Calendar calendar = Calendar.getInstance();
+String dataatual = formatter.format(calendar.getTime());
+Nome = strNome.toString();
+System.out.println(Nome+1);
+strNome.append("Barra");
+strNome.append(dataatual);
+strNome.append(".png");
+
+Nome = strNome.toString();
+System.out.println(Nome);
+
+
+
+
+
+
+// Gera o gráfico própriamente  
+ChartUtilities.writeChartAsPNG(new FileOutputStream(Nome, true), chart, 400, 200, null, false, 0);  
+System.out.println(Nome);
 ChartPanel panel = new ChartPanel(chart);
 panel.setPreferredSize(new Dimension(400, 300));
 setContentPane(panel);
@@ -62,8 +96,8 @@ return dataset;
 
 private static JFreeChart createBarChart(CategoryDataset dataset) {
 JFreeChart chart = ChartFactory.createBarChart(
-"Escolha de cor por veículo", //Titulo
-"Veículo", // Eixo X
+"Serviços", //Titulo
+"Serviços", // Eixo X
 "Quantidade", //Eixo Y
 dataset, // Dados para o grafico
 PlotOrientation.VERTICAL, //Orientacao do grafico
@@ -71,7 +105,9 @@ true, false, false); // exibir: legendas, tooltips, url
 return chart;
 }
 
-public static void main( String[] args ) throws ClassNotFoundException, SQLException {
+
+
+public static void main( String[] args ) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
 	PanelGraficoBarras chart = new PanelGraficoBarras("Teste Bar Chart");
 chart.pack();
 chart.setVisible(true);
